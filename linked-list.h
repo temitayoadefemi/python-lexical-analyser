247835b2b94b084d9aeaad3f1ff0a1f0b19072e8
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-21 10:25:54 +0100

    Update linked-list.h

--- linked-list.h
+++ linked-list.h
@@ -1,18 +1,9 @@
 #include <stdio.h>     // Standard input/output library (for printf, etc.)
 #include <stdlib.h>    // Standard library (for malloc, free, etc.)
 
-
 typedef struct ListNode {
-
-    int key;
-    int value;
-    struct ListNode *next;
-    struct ListNode *prev;
-
-
+    char* value;
+    struct ListNode* next;
 } ListNode;
 
-typedef struct LinkedList {
-    struct ListNode *head;
-
-} LinkedList;
+ListNode* head = NULL;
